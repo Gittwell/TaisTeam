0104c41e8f6af1e44db57bd10698c29ad6d63291
Maxim Shirokov <gittwell@outlook.com>
2024-08-21 16:33:25 +0300

    add first version

--- taisteam/projects/urls.py
+++ taisteam/projects/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('notice/', views.notice, name='notice'),
     path('cats/', views.categories, name='categories'),
     path('about/', views.about, name='about'),
 ]
--- taisteam/projects/views.py
+++ taisteam/projects/views.py
@@ -9,10 +9,59 @@ data = {'categories': [{'cat': 'Home', 'img': '#home'},
                        {'cat': 'Работники', 'img': "#people-circle"}],
         'drop_menu': ['New project...', 'Settings', 'Profile', 'Sign out']}
 
+
+notice_data = {"table":[
+    {'number': '1', 'title': 'ТС-245-24', 'about': 'Корректировка ТС-6519-0ГЧ', 'date': '31.07.2024', 'surname': 'Широков'},
+    {'number': '2', 'title': 'ТС-246-24', 'about': 'Выпускается вновь ТС-6519-0Э6', 'date': '31.07.2024', 'surname': 'Ермилов'},
+    {'number': '3', 'title': 'ТС-247-24', 'about': 'Выпускается вновь ТС-6519-0ПС', 'date': '31.07.2024', 'surname': 'Кузнецов'},
+    {'number': '#', 'title': 'Обозначение', 'about': 'О чем', 'date': 'Дата', 'surname': 'Фамилия'},
+    {'number': '1', 'title': 'ТС-245-24', 'about': 'Корректировка ТС-6519-0ГЧ', 'date': '31.07.2024',
+     'surname': 'Широков'},
+    {'number': '2', 'title': 'ТС-246-24', 'about': 'Выпускается вновь ТС-6519-0Э6', 'date': '31.07.2024',
+     'surname': 'Ермилов'},
+    {'number': '3', 'title': 'ТС-247-24', 'about': 'Выпускается вновь ТС-6519-0ПС', 'date': '31.07.2024',
+     'surname': 'Кузнецов'},
+    {'number': '#', 'title': 'Обозначение', 'about': 'О чем', 'date': 'Дата', 'surname': 'Фамилия'},
+    {'number': '1', 'title': 'ТС-245-24', 'about': 'Корректировка ТС-6519-0ГЧ', 'date': '31.07.2024',
+     'surname': 'Широков'},
+    {'number': '2', 'title': 'ТС-246-24', 'about': 'Выпускается вновь ТС-6519-0Э6', 'date': '31.07.2024',
+     'surname': 'Ермилов'},
+    {'number': '3', 'title': 'ТС-247-24', 'about': 'Выпускается вновь ТС-6519-0ПС', 'date': '31.07.2024',
+     'surname': 'Кузнецов'},
+    {'number': '#', 'title': 'Обозначение', 'about': 'О чем', 'date': 'Дата', 'surname': 'Фамилия'},
+    {'number': '1', 'title': 'ТС-245-24', 'about': 'Корректировка ТС-6519-0ГЧ', 'date': '31.07.2024',
+     'surname': 'Широков'},
+    {'number': '2', 'title': 'ТС-246-24', 'about': 'Выпускается вновь ТС-6519-0Э6', 'date': '31.07.2024',
+     'surname': 'Ермилов'},
+    {'number': '3', 'title': 'ТС-247-24', 'about': 'Выпускается вновь ТС-6519-0ПС', 'date': '31.07.2024',
+     'surname': 'Кузнецов'},
+    {'number': '#', 'title': 'Обозначение', 'about': 'О чем', 'date': 'Дата', 'surname': 'Фамилия'},
+    {'number': '1', 'title': 'ТС-245-24', 'about': 'Корректировка ТС-6519-0ГЧ', 'date': '31.07.2024',
+     'surname': 'Широков'},
+    {'number': '2', 'title': 'ТС-246-24', 'about': 'Выпускается вновь ТС-6519-0Э6', 'date': '31.07.2024',
+     'surname': 'Ермилов'},
+    {'number': '3', 'title': 'ТС-247-24', 'about': 'Выпускается вновь ТС-6519-0ПС', 'date': '31.07.2024',
+     'surname': 'Кузнецов'},
+    {'number': '#', 'title': 'Обозначение', 'about': 'О чем', 'date': 'Дата', 'surname': 'Фамилия'},
+    {'number': '1', 'title': 'ТС-245-24', 'about': 'Корректировка ТС-6519-0ГЧ', 'date': '31.07.2024',
+     'surname': 'Широков'},
+    {'number': '2', 'title': 'ТС-246-24', 'about': 'Выпускается вновь ТС-6519-0Э6', 'date': '31.07.2024',
+     'surname': 'Ермилов'},
+    {'number': '3', 'title': 'ТС-247-24', 'about': 'Выпускается вновь ТС-6519-0ПС', 'date': '31.07.2024',
+     'surname': 'Кузнецов'},
+]
+}
+
 def index(request):
     return render(request, 'projects/index.html', data)
 
+
+def notice(request):
+    return render(request, 'projects/notice.html', notice_data)
+
+
 def about(request):
     return render(request, 'projects/about.html')
+
 def categories(request):
     return HttpResponse("<h1>Статьи по категориям</h1>")
